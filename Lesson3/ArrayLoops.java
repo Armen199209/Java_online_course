9fbfabc9000a1637cc24aec593e944726b06225f
avagyan.armen <avagyan.armen@wb.ru>
2024-09-14 13:27:44 +0400

    Lesson3 Loops

---
+++ Lesson3/ArrayLoops.java
@@ -0,0 +1,33 @@
+package Lesson3;
+
+public class ArrayLoops {
+
+    public static void main(String[] args) {
+        int[] x;
+
+        x = new int[]{1, 2, 3, 4, 5};
+
+        System.out.println(x[0]);
+
+        int y = 1;
+
+        if (y > 0) {
+            System.out.println("Positive");
+        } else {
+            if (y == 0) {
+                System.out.println("Zero");
+            } else {
+                System.out.println("Negative");
+            }
+        }
+
+
+        System.out.println("-----------------------------------------------");
+
+        String[] names = {"Anna", "Karen", "Marine", "Hayk", "Vazgen"};
+        for (int i = names.length-1; i >= 0; i--) {
+            System.out.println(names[i]);
+
+        }
+    }
+}
